a47ddef52b9d676d2b26107947b4d85bde0ea0ff
stevenJSP <stevenJSP@gitee.com>
2025-06-17 14:15:48 +0800

    第三次

---
+++ lenth.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class lenth {
+    public static void main(String[] args){
+        int foot;//英尺
+        int inch;//英寸
+        Scanner in = new Scanner(System.in);
+        foot = in.nextInt();
+        inch = in.nextInt();
+        System.out.println((foot+inch/12.0)*0.3048);
+    }
+
+
+}
