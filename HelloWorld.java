48a806b4ea66c47cd5c972efeb5716dadb505f4e
stevenJSP <stevenJSP@gitee.com>
2025-06-17 10:23:43 +0800

    第二次

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class HelloWorld{
 	public static void main(String[] args){
-			System.out.println("HelloWorld");
+//			System.out.println("HelloWorld");
+		System.out.println(2+3+"=2+3="+(2+3));
+		Scanner in = new java.util.Scanner(System.in);
+		System.out.println(in.nextLine());
+
 	}
 
 }
