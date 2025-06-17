edfb3e9c64769deb21a5bb01d274d5f79c2fcfe6
stevenJSP <stevenJSP@gitee.com>
2025-06-17 14:14:08 +0800

    第三次

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,12 @@ public class HelloWorld{
 //			System.out.println("HelloWorld");
 		System.out.println(2+3+"=2+3="+(2+3));
 		Scanner in = new java.util.Scanner(System.in);
-		System.out.println(in.nextLine());
+//		System.out.println(in.nextLine());
+		System.out.print("请输入票面:");
+		int amount = in.nextInt();
+		System.out.print("请输入金额:");
+		int price = in.nextInt();
+		System.out.println(amount+"-"+price+"="+(amount-price));
 
 	}
 
